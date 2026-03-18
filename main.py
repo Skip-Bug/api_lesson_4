967e908e3a82d5dad4f2af61405293d746210624
Leonid Stoyko <gribojor@gmail.com>
2026-03-18 16:23:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
+from pathlib import Path
 import requests
-import os
 
 
 def main():
-    folder_name = 'images'
+
     filename = 'hubble.jpeg'
-    full_name = os.path.join(folder_name, filename)
+    folder_path = Path('images')
+    folder_path.mkdir(parents=True, exist_ok=True)
+    full_path = folder_path / filename
+
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
     headers = {
         'User-Agent': (
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-            'AppleWebKit/537.36 (KHTML, like Gecko)'
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/145.0.0.0 Safari/537.36'
         )
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
 
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    with open(f"{full_name}", 'wb') as file:
-        file.write(response.content)
+    # with open(full_path, 'wb') as file:
+    #     file.write(response.content)
+    full_path.write_bytes(response.content)
 
 
 if __name__ == '__main__':
