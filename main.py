c538a823bd78f373f2f3dc6473b79564a6043243
Leonid Stoyko <gribojor@gmail.com>
2026-03-18 12:20:38 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def main():
+    filename = 'hubble.jpeg'
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    headers = {
+        'User-Agent': (
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            'AppleWebKit/537.36 (KHTML, like Gecko)'
+            'Chrome/145.0.0.0 Safari/537.36'
+        )
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    main()
