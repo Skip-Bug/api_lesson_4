f5694dedcc64f98ed5f0a4082741272cff154569
Leonid Stoyko <gribojor@gmail.com>
2026-03-22 13:37:48 +0500

    get  NASA EPIC API links

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from urllib.parse import urlsplit, unquote
 from os.path import split, splitext
 import requests
 import os
+from pprint import pprint
 
 
 def get_extension(url):
@@ -20,7 +21,50 @@ def get_extension(url):
     return extension
 
 
-def get_links_nasa(api_key=None, count=None, date=None, hd=False):
+def get_links_nasa_epic(api_key=None, date=None):
+    """Получает список ссылок на изображения с NASA EPIC API.
+
+    Args:
+        api_key (str, optional): API-ключ NASA. По умолчанию "DEMO_KEY".
+        date (str, optional): Дата в формате YYYY-MM-DD.
+
+    Returns:
+        list: Список строк с URL изображений.
+            Может быть пустым, если ссылки не найдены. 
+    """
+    if api_key is None:
+        api_key = "DEMO_KEY"
+
+    params = {'api_key': api_key}
+
+    if date:
+        base_url = f"https://api.nasa.gov/EPIC/api/natural/images/date/{date}"
+    else:
+        base_url = "https://api.nasa.gov/EPIC/api/natural/images"
+
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    epic_response = response.json()
+    # отладка
+    print(f"Найдено записей: {len(epic_response)}")
+    if epic_response:
+        print("Первый элемент:")
+        pprint(epic_response[0])
+    # отладка не забудь удалить
+    some_links = []
+    base_photo_url = "https://api.nasa.gov/EPIC/archive/natural/"
+    for epic_link in epic_response:
+        date_str = epic_link['date'].split()[0].replace('-', '/')
+        name_photo = epic_link['image']
+
+        photo_url = f"{base_photo_url}{date_str}/png/{name_photo}.png"
+
+        some_links.append(photo_url)
+
+    return some_links
+
+
+def get_links_nasa_apod(api_key=None, count=None, date=None, hd=False):
     """Получает список ссылок на изображения с NASA APOD API.
 
     Args:
@@ -48,6 +92,7 @@ def get_links_nasa(api_key=None, count=None, date=None, hd=False):
         params['hd'] = True
 
     base_url = "https://api.nasa.gov/planetary/apod"
+
     response = requests.get(base_url, params=params)
     response.raise_for_status()
 
@@ -70,7 +115,6 @@ def get_links_nasa(api_key=None, count=None, date=None, hd=False):
 
         if some_link:
             some_links.append(some_link)
-
     return some_links
 
 
@@ -116,44 +160,44 @@ def ensure_list(some_links):
     return [some_links]
 
 
-def download_image(url, name_photo, path, number_photo=None, headers=None):
-    """Сохраняет картинку по URL в указанную папку.
+# def download_image(url, name_photo, path, number_photo=None, headers=None):
+#     """Сохраняет картинку по URL в указанную папку.
 
-    Возвращает путь к сохранённому файлу.
+#     Возвращает путь к сохранённому файлу.
 
-    Args:
-        url (str): Адрес картинки в интернете.
-        name_photo (str): Название фото.
-        path (str): Папка для сохранения (будет создана, если нет).
-        number_photo (int, optional): Номер фото для сохранения
-            (если фото одно то сохранится по названию если,
-             их несколько то название_1 и т.д.).
-        headers (dict, optional): Заголовки HTTP (если None,
-         используется стандартный User-Agent).
+#     Args:
+#         url (str): Адрес картинки в интернете.
+#         name_photo (str): Название фото.
+#         path (str): Папка для сохранения (будет создана, если нет).
+#         number_photo (int, optional): Номер фото для сохранения
+#             (если фото одно то сохранится по названию если,
+#              их несколько то название_1 и т.д.).
+#         headers (dict, optional): Заголовки HTTP (если None,
+#          используется стандартный User-Agent).
 
-    Returns:
-        Path: Путь к сохраненному файлу.
-    """
-    if headers is None:
-        headers = {
-            'User-Agent': (
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                'AppleWebKit/537.36 (KHTML, like Gecko) '
-                'Chrome/145.0.0.0 Safari/537.36'
-            )
-        }
-    folder_path = Path(path)
-    folder_path.mkdir(parents=True, exist_ok=True)
-    extension = get_extension(url)
-    if number_photo is None:
-        full_path = folder_path / f"{name_photo}{extension}"
-    else:
-        full_path = folder_path / f"{name_photo}_{number_photo}{extension}"
+#     Returns:
+#         Path: Путь к сохраненному файлу.
+#     """
+#     if headers is None:
+#         headers = {
+#             'User-Agent': (
+#                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+#                 'AppleWebKit/537.36 (KHTML, like Gecko) '
+#                 'Chrome/145.0.0.0 Safari/537.36'
+#             )
+#         }
+#     folder_path = Path(path)
+#     folder_path.mkdir(parents=True, exist_ok=True)
+#     extension = get_extension(url)
+#     if number_photo is None:
+#         full_path = folder_path / f"{name_photo}{extension}"
+#     else:
+#         full_path = folder_path / f"{name_photo}_{number_photo}{extension}"
 
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    full_path.write_bytes(response.content)
-    return full_path
+#     response = requests.get(url, headers=headers)
+#     response.raise_for_status()
+#     full_path.write_bytes(response.content)
+#     return full_path
 
 
 def main():
@@ -162,14 +206,16 @@ def main():
     load_dotenv()
     name_photo = input("Введите название фото:").strip().lower()
     # spacex_id = os.getenv('SPACEX_ID')
-    api_key = os.getenv('NASA_ID')
+    api_key = None  # os.getenv('NASA_ID')
 
     path = input("Введите путь: ").strip() or 'images/'
     try:
-        some_links = get_links_nasa(api_key, count=30)
-        links_photo = ensure_list(some_links)
+
+        some_links = get_links_nasa_epic(api_key)
+        # some_links = get_links_nasa_apod(api_key)
+
         # some_links = get_links_spacex(spacex_id)
-        # links_photo = ensure_list(some_links)
+        links_photo = ensure_list(some_links)
     except requests.exceptions.ReadTimeout:
         print("Превышено время ожидания...")
         return
