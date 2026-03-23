20dd65c1b37c726f9e8aee8a3d09ef7037f6b9a1
Leonid Stoyko <gribojor@gmail.com>
2026-03-23 23:20:37 +0500

    publish photo publishes with a configurable delay

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
 from telegram import Bot
 from telegram.utils.request import Request
 from dotenv import load_dotenv
+import argparse
+import random
+import time
 import os
 
 
+def time_parser():
+    """Дает возможность выбирать задержку публикаций."""
+    parser = argparse.ArgumentParser(
+        description='Запускает работу бота с задержкой'
+    )
+    parser.add_argument('sleep',
+                        nargs='?',
+                        default='14400',
+                        help='Задержка публикаций (по умолчанию 4 часа)'
+                        )
+    return parser
+
+
 def main():
+    """ Запускает бот который опубликует изображения.
+
+    Запускает телеграмм Бота , котрый с задержкой в 4 часа
+    или заданной пользователем опубликует изображения из папки images.
+    Когда вес изображения отправлены Бот перемешает все и начнет заного.
+    Если удалить все изображения бот будет ждать загрузки новых файлов.
+    Ctrl+C остановит работу Бота
+    """
     load_dotenv()
+    images_folder = "images"
+    channel_id = os.getenv("TG_CHANNEL_ID")
     token = os.getenv('TG_BOT_TOKEN')
     proxy_url = os.getenv('PROXY_URL')
+    try:
+        sleep_seconds = int(args.sleep)
+    except ValueError:
+        print("Ошибка: задержка задается в секундах")
+        return
+    if not channel_id:
+        print("Канал не обнаружен")
+        return
     if not token:
         print(" Токен не найден в .env")
         return
 
+    parser = time_parser()
+    args = parser.parse_args()
     request = Request(proxy_url=proxy_url, connect_timeout=20, read_timeout=20)
     bot = Bot(token=token, request=request)
 
-    channel_id = "@load_image"  # "-1003711383466"
+    try:
+        while True:
+            images = os.listdir(images_folder)
+            if not images:
+                print("Папка пуста")
+                time.sleep(sleep_seconds)
+                continue
 
-    file_path = "images/spacex_1.jpg"
-    with open(file_path, 'rb') as doc:
-        bot.send_document(chat_id=channel_id, document=doc,
-                          caption="Привет от бота!")
+            random.shuffle(images)
+            for image_file in images:
+                image_path = os.path.join(images_folder, image_file)
+                with open(image_path, 'rb') as image:
+                    bot.send_photo(chat_id=channel_id, photo=image,
+                                   caption="Привет от бота!")
+                time.sleep(sleep_seconds)
+
+    except KeyboardInterrupt:
+        print("\nБот остановлен пользователем.")
+        bot.send_message(chat_id=channel_id, text="Бот завершил работу")
+
+    except Exception as exp:
+        print(f"Ошибка: {exp}")
+        bot.send_message(chat_id=channel_id, text="Я сломался")
 
 
 if __name__ == '__main__':
